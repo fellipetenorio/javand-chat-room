f30c3d9116f59198642c5e95f0d1bb00ed30da98
fellipetenorio <3355473ftfN>
2019-09-25 11:51:21 -0300

    handle sessions

--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -25,7 +26,18 @@ public class WebSocketChatServer {
 
     private static void sendMessageToAll(String msg) {
         //TODO: add send message method.
-
+            for (Map.Entry<String, Session> entry : onlineSessions.entrySet()) {
+                Session session = entry.getValue();
+                System.out.println("session::"+entry.getKey()+"::sessions: "+ session.getOpenSessions().size());
+                try {
+                    for (Session sess : session.getOpenSessions()) {
+                        if (sess.isOpen())
+                            sess.getBasicRemote().sendText(msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
     }
 
     /**
@@ -34,7 +46,10 @@ public class WebSocketChatServer {
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("onOpen::"+session.getId());
-        onlineSessions.put(session.getId(), session);
+        List<String> usernames = session.getRequestParameterMap().get("username");
+
+        final String username = usernames.get(0);
+        onlineSessions.put(username, session);
     }
 
     /**
